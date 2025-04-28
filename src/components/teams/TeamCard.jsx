52221b2e6c540322167aa8cd25dc74d8519a3100
Amit jha <amitjha@Amits-MacBook-Pro.local>
2025-04-29 02:15:58 +0530

    added lazy in img

--- src/components/teams/TeamCard.jsx
+++ src/components/teams/TeamCard.jsx
@@ -8,7 +8,12 @@ function TeamCard({ details }) {
   return (
     <div className="team-card">
       <div className="team-image-wrapper" style={{ background: bg }}>
-        <img src={`teams/${name}.png`} className="team-image" alt="" />
+        <img
+          src={`teams/${name}.png`}
+          className="team-image"
+          alt=""
+          loading="lazy"
+        />
       </div>
       <div className="team-name">{name.split("_").join(" ")}</div>
       <div className="team-title">{title}</div>
